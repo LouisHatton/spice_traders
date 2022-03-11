60bff0fed9e6b45983a9ba2587c2f980b4ea4cc0
Potters <13716904+PottersMC@users.noreply.github.com>
2022-03-11 15:04:45 +0000

    Skeleton and basic Asset Tests

---
+++ tests/src/test/java/com/mygdx/pirategame/tests/TestRunner.java
@@ -0,0 +1,36 @@
+package com.mygdx.pirategame.tests;
+
+import com.mygdx.pirategame.tests.impl.AssetTests;
+import com.mygdx.pirategame.tests.impl.TileTests;
+import com.mygdx.pirategame.tests.lib.GdxTestRunner;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+import org.mockito.internal.util.collections.Sets;
+
+import java.util.Set;
+
+@RunWith(GdxTestRunner.class)
+public class TestRunner {
+
+	private final static Set<Class<?>> TESTS_TO_RUN = Sets.newSet(
+			AssetTests.class,
+			TileTests.class
+	);
+
+	@Test
+	public static void main(String[] args) {
+		for (Class<?> clazz : TESTS_TO_RUN) {
+			System.out.println("Running " + clazz.getSimpleName());
+			Result result = JUnitCore.runClasses(clazz);
+
+			for (Failure failure : result.getFailures()) {
+				System.out.println(failure.toString());
+			}
+
+			System.out.println(result.wasSuccessful());
+		}
+	}
+}
---
+++ tests/src/test/java/com/mygdx/pirategame/tests/impl/TileTests.java
@@ -0,0 +1,13 @@
+package com.mygdx.pirategame.tests.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TileTests {
+
+	@Test
+	public void testTiles() {
+		assertEquals(1, 1);
+	}
+}
