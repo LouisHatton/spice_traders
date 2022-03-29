88d152e9af68c4a66506891ae6301d95b892e14a
Potters <13716904+PottersMC@users.noreply.github.com>
2022-03-29 11:57:29 +0100

    Test Runner

--- tests/src/test/java/com/mygdx/pirategame/tests/TestRunner.java
+++ tests/src/test/java/com/mygdx/pirategame/tests/TestRunner.java
@@ -3,17 +3,17 @@ package com.mygdx.pirategame.tests;
 import com.mygdx.pirategame.tests.impl.AssetTests;
 import com.mygdx.pirategame.tests.impl.ScoreTest;
 import com.mygdx.pirategame.tests.impl.TileTests;
-import com.mygdx.pirategame.tests.lib.GdxTestRunner;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
-import org.junit.runner.RunWith;
 import org.junit.runner.notification.Failure;
+import org.junit.runners.Suite;
 import org.mockito.internal.util.collections.Sets;
 
 import java.util.Set;
 
-@RunWith(GdxTestRunner.class)
+@Suite.SuiteClasses({AssetTests.class, ScoreTest.class, TileTests.class})
 public class TestRunner {
 
 	private final static Set<Class<?>> TESTS_TO_RUN = Sets.newSet(
@@ -22,7 +22,10 @@ public class TestRunner {
 			ScoreTest.class
 	);
 
-	public static void main(String[] args) {
+	@Test
+	public void runTests() {
+		boolean hasPassed = true;
+
 		for (Class<?> clazz : TESTS_TO_RUN) {
 			System.out.println("Running " + clazz.getSimpleName());
 			Result result = JUnitCore.runClasses(clazz);
@@ -30,11 +33,14 @@ public class TestRunner {
 			for (Failure failure : result.getFailures()) {
 				System.out.println("Failure for " + clazz + " at " + failure.toString());
 				System.out.println(failure.getTrace());
+
+				hasPassed = false;
 			}
 
 			System.out.println("Complete! Ran " + result.getRunCount() + " tests. In total " + (result.getRunCount() - result.getFailureCount()) + " tests passed. Failure for " + result.getFailureCount() + " tests.");
 		}
 
 		System.out.println("All tests complete");
+		Assert.assertTrue("Not all tests have passed!", hasPassed);
 	}
 }
